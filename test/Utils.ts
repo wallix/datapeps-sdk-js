9b1dd923c28c1913b6857ad49cf0586fd2a11b21
CI <ci@datapeps.com>
2018-11-21 09:52:00 +0000

    Update SDK to v0.0.42

--- test/Utils.ts
+++ test/Utils.ts
@@ -7,10 +7,10 @@ export function itError(
   description: string,
   action: () => Promise<any>,
   kind: DataPeps.ErrorKind,
-  payload?: any
+  payload?: () => any
 ): mocha.ITest {
   return it(`${description} expect error(${kind})`, async () =>
-    await expectError(action(), kind, payload));
+    await expectError(action(), kind, payload != null ? payload() : null));
 }
 
 export async function expectError(
@@ -25,9 +25,118 @@ export async function expectError(
     expect(e).instanceof(DataPeps.Error);
     expect(e.kind).equal(kind);
     if (payload != null) {
-      expect(payload).deep.equals(e.payload);
+      expect(payload).to.deep.equals({ ...e.payload });
     }
     return;
   }
   throw new Error(`action should throw a DataPepsError(${kind})`);
+}
+
+// CLASSES
+
+export type TestResource = DataPeps.Resource<{ description: string }>;
+export class ResourceContent {
+  plain: Uint8Array;
+  encrypted: Uint8Array;
+
+  constructor(resource: DataPeps.Resource<{}>, content: string) {
+    let textEncoder = new TextEncoder();
+    this.plain = textEncoder.encode(content);
+    this.encrypted = resource.encrypt(this.plain);
+  }
+}
+
+export class Resource {
+  resource: DataPeps.Resource<{}>;
+  content: ResourceContent;
+
+  constructor(resource: DataPeps.Resource<{}>, content: string) {
+    this.resource = resource;
+    this.content = new ResourceContent(resource, content);
+  }
+}
+
+// FUNCTIONS
+
+export function sleep(ms: number): Promise<void> {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+export async function wait(
+  ms: number,
+  predicate: () => boolean
+): Promise<boolean> {
+  while (ms > 0 && !predicate()) {
+    ms -= 10;
+    await sleep(10);
+  }
+  return ms > 0;
+}
+
+// TRYING FUNCTIONS
+
+export async function tryAndCheckIdentityNotFoundError(
+  session: DataPeps.Session,
+  id: DataPeps.ID
+) {
+  let errorOccurred = { isTrue: false };
+  try {
+    await session.Resource.get<TestResource>(id);
+  } catch (err) {
+    checkIdentityNotFoundError(err, errorOccurred);
+  }
+  expect(errorOccurred.isTrue).to.be.true;
+}
+
+// CHECKING FUNCTIONS
+
+export function checkFetchedResource(
+  resourceFetched: DataPeps.Resource<{}>,
+  resourceExpected: Resource
+) {
+  expect(resourceFetched).to.not.be.null;
+  expect(resourceFetched.id).to.be.deep.equals(resourceExpected.resource.id);
+  expect(resourceFetched.payload).to.be.deep.equals(
+    resourceExpected.resource.payload
+  );
+  let decryptedContent = resourceFetched.decrypt(
+    resourceExpected.content.encrypted
+  );
+  expect(decryptedContent).to.not.be.null;
+  expect(decryptedContent).to.be.deep.equals(resourceExpected.content.plain);
+}
+
+export function checkResourceNotFoundError(
+  err,
+  resourceId: DataPeps.ID,
+  errorOccurred: { isTrue: boolean }
+) {
+  expect(err).to.not.be.null;
+  expect(err).instanceof(DataPeps.Error);
+  expect(err.kind).equal(DataPeps.ServerError.ResourceNotFound);
+  expect(err.payload.id).to.be.deep.equals(resourceId);
+  errorOccurred.isTrue = true;
+}
+
+export function checkIdentityNotFoundError(
+  err,
+  errorOccurred: { isTrue: boolean }
+) {
+  expect(err).to.not.be.null;
+  expect(err).instanceof(DataPeps.Error);
+  expect(err.kind).equal(DataPeps.ServerError.IdentityNotFound);
+  errorOccurred.isTrue = true;
+}
+
+// FETCHING FUNCTIONS
+
+export async function fetchAndCheckResource(
+  session: DataPeps.Session,
+  resource: Resource
+): Promise<DataPeps.Resource<{}>> {
+  let resourceFecthed = await session.Resource.get(resource.resource.id);
+  checkFetchedResource(resourceFecthed, resource);
+  return Promise.resolve(resourceFecthed);
 }
--- test/integration/identity/AccessGroup.ts
+++ test/integration/identity/AccessGroup.ts
@@ -1,98 +1,105 @@
-import * as Config from '../../Config';
-import * as DataPeps from '../../../src/DataPeps';
-import * as nacl from 'tweetnacl';
-import { expect } from 'chai';
+import * as Config from "../../Config";
+import * as DataPeps from "../../../src/DataPeps";
+import * as nacl from "tweetnacl";
+import { expect } from "chai";
 
 /**
  * This test is about testing of the funtion DataPeps.IdentityAPI.getAccessGroup
  */
-describe('identity.AccessGroup', () => {
-    let seed = Math.floor(Math.random() * 99999)
+describe("identity.AccessGroup", () => {
+  let seed = Math.floor(Math.random() * 99999);
 
-    let aliceSecret = nacl.randomBytes(128)
-    let alice: DataPeps.IdentityFields = {
-        login: "alice." + seed,
-        name: "alice test identity, TS",
-        kind: "user",
-        payload: null,
-    }
+  let aliceSecret = nacl.randomBytes(128);
+  let alice: DataPeps.IdentityFields = {
+    login: "alice." + seed,
+    name: "alice test identity, TS",
+    kind: "user",
+    payload: null
+  };
 
-    function getOtherLogin(i: number) {
-        return "other." + i + "." + seed
-    }
+  function getOtherLogin(i: number) {
+    return "other." + i + "." + seed;
+  }
 
-    let nothers = 10
-    let others = new Array(nothers).fill(0).map((x, i) => {
-        return {
-            login: getOtherLogin(i),
-            name: "Other " + i,
-            kind: "other",
-            payload: null,
-        }
-    })
+  let nothers = 10;
+  let others = new Array(nothers).fill(0).map((x, i) => {
+    return {
+      login: getOtherLogin(i),
+      name: "Other " + i,
+      kind: "other",
+      payload: null
+    };
+  });
 
-    // Register and login alice
-    let aliceSession: DataPeps.Session
-    let adminSession: DataPeps.Session
-    before(async () => {
-        await Config.init()
-        await DataPeps.register(alice, aliceSecret)
-        aliceSession = await DataPeps.login(alice.login, aliceSecret)
-        adminSession = await Config.adminLogin()
-    })
+  // Register and login alice
+  let aliceSession: DataPeps.Session;
+  let adminSession: DataPeps.Session;
+  before(async () => {
+    await Config.init();
+    await DataPeps.register(alice, aliceSecret);
+    aliceSession = await DataPeps.login(alice.login, aliceSecret);
+    adminSession = await Config.adminLogin();
+  });
 
-    it('create others identities with alice in their sharingGroup', async () => {
-        await Promise.all(others.map(other => {
-            return aliceSession.Identity.create(
-                other, { sharingGroup: [alice.login] },
-            )
-        }))
-    })
+  it("create others identities with alice in their sharingGroup", async () => {
+    await Promise.all(
+      others.map(other => {
+        return aliceSession.Identity.create(other, {
+          sharingGroup: [alice.login]
+        });
+      })
+    );
+  });
 
-    async function checkAliceAccessGroup() {
-        let accessGroup = await aliceSession.Identity.getAccessGroup(alice.login)
-        expect(accessGroup.length).to.be.equals(nothers)
-        others.forEach(other => {
-            let link = accessGroup.find((link) => link.id.login == other.login)
-            expect(link).to.not.be.null
-            expect(link.id.login).to.be.equal(other.login)
-            expect(link.locked).to.be.equal(false)
-        })
-    }
+  async function checkAliceAccessGroup() {
+    let accessGroup = await aliceSession.Identity.getAccessGroup(alice.login);
+    expect(accessGroup.length).to.be.equals(nothers);
+    others.forEach(other => {
+      let link = accessGroup.find(link => link.id.login == other.login);
+      expect(link).to.not.be.null;
+      expect(link.id.login).to.be.equal(other.login);
+      expect(link.locked).to.be.equal(false);
+    });
+  }
 
-    it('checks alice accessGroup', async () => {
-        await checkAliceAccessGroup()
-    })
+  it("checks alice accessGroup", async () => {
+    await checkAliceAccessGroup();
+  });
 
-    it('checks alice accessGroup after renew alice keys', async () => {
-        await aliceSession.renewKeys()
-        await checkAliceAccessGroup()
-    })
+  it("checks alice accessGroup after renew alice keys", async () => {
+    await aliceSession.renewKeys();
+    await checkAliceAccessGroup();
+  });
 
-    it('checks alice accessGroup after renew others keys', async () => {
-        await Promise.all(others.map((other) => {
-            return aliceSession.Identity.renewKeys(other.login)
-        }))
-        await checkAliceAccessGroup()
-    })
+  it("checks alice accessGroup after renew others keys", async () => {
+    await Promise.all(
+      others.map(other => {
+        return aliceSession.Identity.renewKeys(other.login);
+      })
+    );
+    await checkAliceAccessGroup();
+  });
 
-    it('checks accessgroup are unlocked after delegate key renewal', async () => {
-        await aliceSession.Identity.renewKeys(getOtherLogin(0));
-    })
+  it("checks accessgroup are unlocked after delegate key renewal", async () => {
+    await aliceSession.Identity.renewKeys(getOtherLogin(0));
+  });
 
-    it('checks alice accessGroup after admin reset keys', async () => {
-        let newPassword = 'a new password'
-        await adminSession.Admin.overwriteKeys(alice.login, newPassword);
-        aliceSession = await DataPeps.login(alice.login, newPassword)
+  it("checks alice accessGroup after admin reset keys", async () => {
+    let newPassword = "a new password";
+    await adminSession.Admin.overwriteKeys(alice.login, newPassword);
+    aliceSession = await DataPeps.login(alice.login, newPassword);
 
-        // Add new element to accessGroup and check it is unlocked
-        nothers = nothers + 1;
-        let afterKeyRenewalLogin = getOtherLogin(nothers)
-        await aliceSession.Identity.create({
-            login: afterKeyRenewalLogin,
-            name: "Other " + nothers,
-            kind: "other",
-            payload: null,
-        }, { sharingGroup: [alice.login] })
-    })
-})
+    // Add new element to accessGroup and check it is unlocked
+    nothers = nothers + 1;
+    let afterKeyRenewalLogin = getOtherLogin(nothers);
+    await aliceSession.Identity.create(
+      {
+        login: afterKeyRenewalLogin,
+        name: "Other " + nothers,
+        kind: "other",
+        payload: null
+      },
+      { sharingGroup: [alice.login] }
+    );
+  });
+});

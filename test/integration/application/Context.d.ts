a8bfdeb765be3732ca66d8138c06ecdbb9727b4c
CI <ci@datapeps.com>
2019-01-24 09:51:54 +0000

    Update SDK to 0.0.44-dev-545b52

--- test/integration/application/Context.d.ts
+++
@@ -1,8 +0,0 @@
-import { JWTConfig } from "./Utils";
-import { initCtx, devCtx, identityOptions, identitiesCtx } from "../../Context";
-/**
- * Create a devCtx with n applications that are configured with all different
- * JWT configs + 1 application that is not configured.
- */
-export declare function devWithAllConfigs(init: initCtx): Promise<devCtx>;
-export declare function registerIdentitiesForEachApps(init: initCtx, dev: devCtx, configs: JWTConfig[], n: number, options?: identityOptions): Promise<identitiesCtx[]>;
--- test/integration/identity/utils.d.ts
+++
@@ -1,4 +0,0 @@
-import { Identity, IdentitySortingField, IdentitySortingOrder } from "../../../src/DataPeps";
-export declare function expectContainsAllIdentities(expected: Identity<Uint8Array>[], result: Identity<Uint8Array>[], both?: boolean): void;
-export declare function expectIdentitiesListAreEquals(expected: Identity<Uint8Array>[], result: Identity<Uint8Array>[]): void;
-export declare function sortIdentities(sortingField: IdentitySortingField, sortingOrder: IdentitySortingOrder, identities: Identity<any>[]): Identity<any>[];
